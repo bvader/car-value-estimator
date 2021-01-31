1d389f8de15be1fab3283ade8d36bea4aeba6bcb
bvader <stephen.brown@elastic.co>
2021-01-31 14:10:49 -0800

    Update versions

--- run-with-apm.sh
+++ run-with-apm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # set -x
 
-AGENT_VERSION=1.12.0
+AGENT_VERSION=1.19.0
 AGENT_FILE=elastic-apm-agent-${AGENT_VERSION}.jar
 
 if [ ! -f "${AGENT_FILE}" ]; then
@@ -14,9 +14,11 @@ java -javaagent:./${AGENT_FILE} \
 -Delastic.apm.server_urls="http://localhost:8200" \
 -Delastic.apm.secret_token="mysecrettoken" \
 -Delastic.apm.service_name="car-value-estimator" \
--Delastic.apm.trace_methods="com.bvader.estimator.*" \
 -Delastic.apm.enable_log_correlation=true \
 -jar target/car-value-estimator-service-0.1.0.jar
 
 # Add this to above if you have a self signed cert for testing 
+#-Delastic.apm.profiling_inferred_spans_enabled=true \
 #-Delastic.apm.verify_server_cert=false \
+#-Delastic.apm.trace_methods="com.bvader.estimator.*" \
+#-Delastic.apm.transaction_sample_rat="0.5" \
